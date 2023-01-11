ecc3d8110d29d994cd0cabbae2febeb917a2a2fb
Ivan Ukhov <ivan.ukhov@gmail.com>
2023-01-11 12:51:31 +0000

    Build an autoencoder

--- explorer/source/data.py
+++ explorer/source/data.py
@@ -10,9 +10,10 @@ def read(
     split: float = 0.2,
     batch_size: int = 32,
     image_size: Tuple[int, int] = (224, 224),
+    image_scale: int = 225,
     random_seed: int = 42,
 ) -> tf.data.Dataset:
-    return tf.keras.utils.image_dataset_from_directory(
+    dataset = tf.keras.utils.image_dataset_from_directory(
         directory=path,
         labels=None,
         color_mode="grayscale",
@@ -22,4 +23,7 @@ def read(
         subset=mode,
         shuffle=True,
         seed=random_seed,
+    )
+    return dataset.prefetch(tf.data.AUTOTUNE).map(
+        lambda image: (image / image_scale, image / image_scale)
     )
--- explorer/source/model.py
+++ explorer/source/model.py
@@ -1,11 +1,75 @@
-from typing import Tuple
+import os
+from typing import List
 
+import aim
+import aim.sdk.adapters.keras_mixins
 import tensorflow as tf
+
+# pylint: disable=too-few-public-methods
+class Model(tf.keras.models.Model):
+    def __init__(self, encoder: List[dict], decoder: List[dict]) -> "Self":
+        super().__init__()
+        self.encoder = tf.keras.Sequential(
+            [tf.keras.layers.Conv2D(**options) for options in encoder],
+            name="encoder",
+        )
+        self.decoder = tf.keras.Sequential(
+            [tf.keras.layers.Conv2DTranspose(**options) for options in decoder],
+            name="decoder",
+        )
+
+    def call(self, data: tf.Tensor) -> tf.keras.models.Model:
+        return self.decoder(self.encoder(data))
+
+
+class AimCallback(
+    aim.sdk.adapters.keras_mixins.TrackerKerasCallbackMetricsEpochEndMixin,
+    tf.keras.callbacks.Callback,
+):
+    def __init__(self, run: aim.Run) -> "Self":
+        # pylint: disable=bad-super-call
+        super(tf.keras.callbacks.Callback, self).__init__()
+        self._run = run
+
+    def close(self) -> None:
+        if self._run:
+            self._run.close()
+            del self._run
+
+    def __del__(self) -> None:
+        self.close()
 
 
 def train(
     data_training: tf.data.Dataset,
     data_validation: tf.data.Dataset,
-    image_size: Tuple[int, int, int],
+    experiment: aim.Run,
+    epochs: int = 100,
+    **options,
 ) -> tf.keras.Model:
-    image = tf.keras.layers.Input(shape=image_size, name="image")
+    model = Model(**options)
+    model.compile(
+        optimizer="adam",
+        loss=tf.keras.losses.MeanSquaredError(),
+    )
+    aim_callback = AimCallback(experiment)
+    earlystopping_callback = tf.keras.callbacks.EarlyStopping(
+        restore_best_weights=True,
+        monitor="val_loss",
+        patience=10,
+    )
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(
+        log_dir=os.path.join(".tensorboard", experiment.hash),
+        histogram_freq=1,
+    )
+    model.fit(
+        data_training,
+        validation_data=data_validation,
+        epochs=epochs,
+        callbacks=[
+            aim_callback,
+            earlystopping_callback,
+            tensorboard_callback,
+        ],
+    )
+    return model
